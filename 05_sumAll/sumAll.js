a5c06b9332018f66e8822d5e559ffb3137650bf9
Caleb Taylor <calebtaylor@gmail.com>
2022-08-31 17:20:53 -0400

    Add sumAll solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  if(typeof a !== 'number' || 
+     typeof b !== 'number' ||
+     a < 0 ||
+     b < 0){
+    return 'ERROR';
+  }
 
+  let low = 0;
+  let stop = 0;
+  let sum = 0;
+  if(a <= b){
+    low = a;
+    stop = b + 1;
+  } else{
+    low = b;
+    stop = a + 1;
+  }
+  for(let i = low; i < stop; i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
