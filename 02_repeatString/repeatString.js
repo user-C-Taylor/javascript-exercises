2d6eed8f574cd7478bd812084e468bcdab0f4102
Caleb Taylor <calebtaylor@gmail.com>
2022-08-31 13:52:03 -0400

    Add solutions for exercises 1 to 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, n) {
+  let repeated = '';
+  if(n < 0){
+    repeated = 'ERROR';
+  } else if(string === ''){
+    // Return below with repeated = ''.
+  }else {
+    for(let i = 0; i < n; i++){
+      repeated += string;
+    }
+  }
+  return repeated;
 };
 
 // Do not edit below this line
