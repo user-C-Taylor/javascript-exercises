bd7c276ca508c1fd4e2f19aa8633652dc0fca5be
Caleb Taylor <calebtaylor@gmail.com>
2022-08-31 18:14:32 -0400

    Add tempConversion solution

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,21 @@
-const ftoc = function() {
+// Take in fahrenheit temperature.
+// Convert to celsius temperature.
+// Round to one decimal place.
 
+// Take in celsius temperature.
+// Convert to fahrenheit temperature.
+// Round to one decimal place.
+
+const ftoc = function(fahr) {
+  const celsius = (fahr - 32) * (5 / 9);
+  const roundCel = +celsius.toFixed(1);
+  return roundCel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  const fahrenheit = (cel * (9 / 5)) + 32;
+  const roundFahr = +fahrenheit.toFixed(1);
+  return roundFahr;
 };
 
 // Do not edit below this line
