e748ef8f0ef5791b48a5dc8ad3677c31e9f19d37
Caleb Taylor <calebtaylor@gmail.com>
2022-08-31 17:40:53 -0400

    Add leapYears solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,20 @@
-const leapYears = function() {
+// check 
+//   IF year is divisible by four and NOT divisible by 100
+//     return true
+//   IF year is divisible by 400
+//     return true
+//   ELSE
+//     return false
 
+const leapYears = function(year) {
+  if(year % 4 === 0 && 
+     year % 100 !== 0){
+      return true;
+  } else if(year % 400 === 0){
+      return true;
+  } else{
+      return false;
+  }
 };
 
 // Do not edit below this line
